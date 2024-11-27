ff206ce8a0b8c81b027990fc385990a1c851a94b
murtazarangwala99 <murtazarangwala95@gmail.com>
2024-11-27 22:27:14 +0530

    vercel bug fixes

--- server.js
+++ server.js
@@ -13,7 +13,9 @@ const apiRoutes = require("./api/routes");
 app.use("/", apiRoutes);
 
 // Start the Server
-const PORT = 5000;
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+// const PORT = 5000;
+// app.listen(PORT, () => {
+//   console.log(`Server is running on http://localhost:${PORT}`);
+// });
+
+module.exports = app;
